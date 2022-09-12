0810dc8cd188fd3cb8da0e25097ec710d5baabec
Joonas Köppä <joonas.koppa@smartly.io>
2022-09-12 19:56:18 +0300

    Initial commit

---
+++ ethereum/compile.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fs.readFileSync(campaignPath, 'utf8');
+
+const compilerInput = {
+    language: "Solidity",
+    sources: {
+        [campaignPath]: {
+            content: source
+        } 
+    },
+    settings: {
+        outputSelection: {
+            "*": {
+                "*": ["abi", "evm.bytecode.object"],
+            },
+        },
+    },
+};
+
+console.log("Compiling Campaign.sol...");
+const rawCompiled = solc.compile(JSON.stringify(compilerInput));
+const compiled = JSON.parse(rawCompiled);
+
+fs.ensureDirSync(buildPath);
+
+for (let contract in compiled.contracts[campaignPath]) {
+    fs.outputJsonSync(
+        path.resolve(buildPath, contract + '.json'),
+        compiled.contracts[campaignPath][contract]
+    );
+}
---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,76 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = address(new Campaign(minimum, msg.sender));
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[] memory) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+
+    struct Request {
+        string description;
+        uint value;
+        address payable recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+    address public manager;
+    uint public minimumContribution;
+    Request[] public requests;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    constructor(uint minimum, address creator) {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        require(!approvers[msg.sender]);
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function createRequest(string memory description, uint value, address payable recipient) public restricted {
+        Request storage r = requests.push();
+        r.description = description;
+        r.value = value;
+        r.recipient = recipient;
+        r.complete = false;
+        r.approvalCount = 0;
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
