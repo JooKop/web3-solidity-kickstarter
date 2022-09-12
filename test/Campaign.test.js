f8aa43f268b3e4be3bc0e3d6a282a573db6c9747
Joonas Köppä <joonas.koppa@smartly.io>
2022-09-12 20:15:02 +0300

    Add initial tests for campaign and campaignfactory

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const options = { gasLimit: 100000000 };
+const web3 = new Web3(ganache.provider(options));
 
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
 const compiledCampaign = require('../ethereum/build/Campaign.json');
@@ -13,9 +14,9 @@ let campaign;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
-    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.abi))
-        .deploy({ data: compiledFactory.evm.bytecode })
-        .send({ from: accounts[0], gas: '1000000' });
+    factory = await new web3.eth.Contract(compiledFactory.abi)
+        .deploy({ data: compiledFactory.evm.bytecode.object })
+        .send({ from: accounts[0], gas: '10000000' });
 
     await factory.methods.createCampaign('100').send({
         from: accounts[0],
@@ -24,9 +25,16 @@ beforeEach(async () => {
 
     [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
 
-    campaign = await Web3.eth.Contract(
-        JSON.parse(compiledCampaign.abi),   
+    campaign = await new web3.eth.Contract(
+        compiledCampaign.abi,
         campaignAddress
     );
 
+});
+
+describe ('Campaigns', () => {
+    it('Deploys a factory and a campaign', () => {
+        assert.ok(factory.options.address);
+        assert.ok(campaign.options.address);
+    });
 });
