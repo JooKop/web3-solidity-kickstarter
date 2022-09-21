7401684eda3c64697f93c839e55da019143ec239
Joonas Köppä <joonas.koppa@gmail.com>
2022-09-21 20:36:24 +0300

    Add getInitialProps and fix formatting

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,43 @@
-import React, { Component } from 'react';
-import factory from '../ethereum/factory';
-import 'semantic-ui-css/semantic.min.css'
-import { Card, Button } from 'semantic-ui-react'
-import Layout from '../components/Layout'
+import React, { Component } from "react";
+import factory from "../ethereum/factory";
+import "semantic-ui-css/semantic.min.css";
+import { Card, Button } from "semantic-ui-react";
+import Layout from "../components/Layout";
 
 class CampaignIndex extends Component {
-    static async getInitialProps() {
-        const campaigns = await factory.methods.getDeployedCampaigns().call();
-        return { campaigns };
-    }
+  static async getInitialProps() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
+    return { campaigns };
+  }
 
-    renderCampaigns() {
-        const items = this.props.campaigns.map(address => {
-            return {
-                header: address,
-                description: <a>View Campaign</a>,
-                fluid: true
-            }
-        })
+  renderCampaigns() {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
 
-        return <Card.Group items={items} />
-    }
+    return <Card.Group items={items} />;
+  }
 
-    render() {
-        return (
-        <Layout>
-            <div>
-                <h3>Open Campaigns</h3>
-                {this.renderCampaigns()}
-                <Button content='Create Campaign' icon='add circle' primary />
-            </div>
-        </Layout>
-        );
-    }
+  render() {
+    return (
+      <Layout>
+        <div>
+          <h3>Open Campaigns</h3>
+          <Button
+            floated="right"
+            content="Create Campaign"
+            icon="add circle"
+            primary
+          />
+          {this.renderCampaigns()}
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export default CampaignIndex;
