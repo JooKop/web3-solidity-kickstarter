9589855dee0ab9b66db88ed0dbfabf052efbc30c
Joonas Köppä <joonas.koppa@gmail.com>
2022-09-22 21:27:15 +0300

    Refactor index to use functional components

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
-import factory from "../ethereum/factory";
-import { Card, Button } from "semantic-ui-react";
+import React from "react";
 import Layout from "../components/Layout";
+import { Card, Button } from "semantic-ui-react";
+import factory from "../ethereum/factory";
 
-class CampaignIndex extends Component {
-  static async getInitialProps() {
-    const campaigns = await factory.methods.getDeployedCampaigns().call();
-    return { campaigns };
-  }
+export async function getServerSideProps() {
+  const campaigns = await factory.methods.getDeployedCampaigns().call();
+  return { props: { campaigns } };
+}
 
-  renderCampaigns() {
-    const items = this.props.campaigns.map((address) => {
+const CampaignIndex = (props) => {
+  const renderCampaigns = () => {
+    const items = props.campaigns.map((address) => {
       return {
         header: address,
         description: <a>View Campaign</a>,
@@ -19,24 +19,22 @@ class CampaignIndex extends Component {
     });
 
     return <Card.Group items={items} />;
-  }
+  };
 
-  render() {
-    return (
-      <Layout>
-        <div>
-          <h3>Open Campaigns</h3>
-          <Button
-            floated="right"
-            content="Create Campaign"
-            icon="add circle"
-            primary
-          />
-          {this.renderCampaigns()}
-        </div>
-      </Layout>
-    );
-  }
-}
+  return (
+    <Layout>
+      <div>
+        <h3>Open Campaigns</h3>
+        <Button
+          floated="right"
+          content="Create Campaign"
+          icon="add circle"
+          primary
+        />
+        {renderCampaigns()}
+      </div>
+    </Layout>
+  );
+};
 
 export default CampaignIndex;
