28416512f8335b00078c4c01979cdaaf29ea4bcb
Joonas Köppä <joonas.koppa@gmail.com>
2022-09-23 22:09:51 +0300

    Make style uniform across js files

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -1,27 +1,27 @@
-const path = require('path');
-const solc = require('solc');
-const fs = require('fs-extra');
+const path = require("path");
+const solc = require("solc");
+const fs = require("fs-extra");
 
-const buildPath = path.resolve(__dirname, 'build');
+const buildPath = path.resolve(__dirname, "build");
 fs.removeSync(buildPath);
 
-const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
-const source = fs.readFileSync(campaignPath, 'utf8');
+const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
+const source = fs.readFileSync(campaignPath, "utf8");
 
 const compilerInput = {
-    language: "Solidity",
-    sources: {
-        [campaignPath]: {
-            content: source
-        } 
+  language: "Solidity",
+  sources: {
+    [campaignPath]: {
+      content: source,
     },
-    settings: {
-        outputSelection: {
-            "*": {
-                "*": ["abi", "evm.bytecode.object"],
-            },
-        },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["abi", "evm.bytecode.object"],
+      },
     },
+  },
 };
 
 console.log("Compiling Campaign.sol...");
@@ -31,8 +31,8 @@ const compiled = JSON.parse(rawCompiled);
 fs.ensureDirSync(buildPath);
 
 for (let contract in compiled.contracts[campaignPath]) {
-    fs.outputJsonSync(
-        path.resolve(buildPath, contract + '.json'),
-        compiled.contracts[campaignPath][contract]
-    );
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract + ".json"),
+    compiled.contracts[campaignPath][contract]
+  );
 }
