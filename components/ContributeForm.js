17e4c0536e597c39f53f89a146a4d3ea1f438000
Joonas Köppä <joonas.koppa@gmail.com>
2022-09-24 16:52:54 +0300

    Add reusable Contribution form component

---
+++ components/ContributeForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Form, Input, Message, Button } from "semantic-ui-react";
+
+const ContributeForm = () => {
+  return (
+    <Form>
+      <Form.Field>
+        <label>Amount to contribute</label>
+        <Input label="ether" labelPosition="right" />
+      </Form.Field>
+      <Button primary>Contribute!</Button>
+    </Form>
+  );
+};
+export default ContributeForm;
