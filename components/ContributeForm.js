1bef2d88d15529369bb37e5e9e96a08c90456fbc
Joonas Köppä <joonas.koppa@gmail.com>
2022-09-24 17:23:35 +0300

    Enable user to contribute to a campaign

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Input, Message, Button } from "semantic-ui-react";
+import Campaign from "../ethereum/campaign";
+import web3 from "../ethereum/web3";
 
-const ContributeForm = () => {
+const ContributeForm = ({ address }) => {
+  const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const campaign = Campaign(address);
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(value, "ether"),
+      });
+    } catch (err) {}
+    setLoading(false);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <Form.Field>
         <label>Amount to contribute</label>
-        <Input label="ether" labelPosition="right" />
+        <Input
+          label="ether"
+          labelPosition="right"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
       </Form.Field>
-      <Button primary>Contribute!</Button>
+      <Button type="submit" primary loading={loading}>
+        Contribute!
+      </Button>
     </Form>
   );
 };
