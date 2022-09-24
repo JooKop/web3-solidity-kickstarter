28d5ede6bc293c6532adb3c9641dba552539ebbb
Joonas Köppä <joonas.koppa@gmail.com>
2022-09-24 18:09:24 +0300

    Show error message in case of errors

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useRouter } from "next/router";
 import { Form, Input, Message, Button } from "semantic-ui-react";
 import Campaign from "../ethereum/campaign";
 import web3 from "../ethereum/web3";
@@ -6,10 +7,13 @@ import web3 from "../ethereum/web3";
 const ContributeForm = ({ address }) => {
   const [value, setValue] = useState("");
   const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
 
   const onSubmit = async (event) => {
     event.preventDefault();
     setLoading(true);
+    setErrorMessage("");
     const campaign = Campaign(address);
 
     try {
@@ -18,12 +22,16 @@ const ContributeForm = ({ address }) => {
         from: accounts[0],
         value: web3.utils.toWei(value, "ether"),
       });
-    } catch (err) {}
+      router.reload();
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
     setLoading(false);
+    setValue("");
   };
 
   return (
-    <Form onSubmit={onSubmit}>
+    <Form onSubmit={onSubmit} error={!!errorMessage}>
       <Form.Field>
         <label>Amount to contribute</label>
         <Input
@@ -33,6 +41,7 @@ const ContributeForm = ({ address }) => {
           onChange={(e) => setValue(e.target.value)}
         />
       </Form.Field>
+      <Message error header="Oops!" content={errorMessage} />
       <Button type="submit" primary loading={loading}>
         Contribute!
       </Button>
