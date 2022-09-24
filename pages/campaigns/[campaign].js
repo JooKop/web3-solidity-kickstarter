caa5f8178c2ba2572a68f51573c85ed4eb56d38a
Joonas Köppä <joonas.koppa@gmail.com>
2022-09-24 16:38:24 +0300

    Display campaign information in individual cards

---
+++ pages/campaigns/[campaign].js
@@ -0,0 +1,70 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Layout from "../../components/Layout";
+import { Card } from "semantic-ui-react";
+import Campaign from "../../ethereum/campaign";
+import web3 from "../../ethereum/web3";
+
+export async function getServerSideProps(context) {
+  const address = context.query.campaign;
+  const campaign = Campaign(address);
+  const summary = await campaign.methods.getSummary().call();
+  console.log(JSON.stringify(summary));
+  return {
+    props: {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4],
+    },
+  };
+}
+
+const CampaignShow = (props) => {
+  const {
+    balance,
+    manager,
+    minimumContribution,
+    requestsCount,
+    approversCount,
+  } = props;
+
+  const items = [
+    {
+      header: manager,
+      meta: "Manager address",
+      description: "The manager created this campaign and can create requests.",
+      style: { overflowWrap: "break-word" },
+    },
+    {
+      header: minimumContribution,
+      meta: "Minimum Contribution (wei)",
+      description: "You must contribute at least this much wei to ",
+    },
+    {
+      header: requestsCount,
+      meta: "Number of requests",
+      description: "A request tries to withdraw money from the contract",
+    },
+    {
+      header: approversCount,
+      meta: "Number of approvers",
+      description: "Number of people who have already donated to this campaign",
+    },
+    {
+      header: web3.utils.fromWei(balance, "ether"),
+      meta: "Campaign balance",
+      description:
+        "The balance is how much money this campaign has left to spend",
+    },
+  ];
+
+  return (
+    <Layout>
+      <Card.Group items={items} />;
+    </Layout>
+  );
+};
+
+export default CampaignShow;
