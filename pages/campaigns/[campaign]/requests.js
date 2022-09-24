396b487bee3b39daeee21f8b789181216734fe93
Joonas Köppä <joonas.koppa@gmail.com>
2022-09-24 18:58:26 +0300

    Add a component for showing all requests

--- pages/campaigns/[campaign]/requests.js
+++ pages/campaigns/[campaign]/requests.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Layout from "../../../components/Layout";
+import Link from "next/link";
+import { Button } from "semantic-ui-react";
 
-const RequestIndex = () => {
-  return <h3>Request List</h3>;
+export async function getServerSideProps(context) {
+  const address = context.query.campaign;
+  return {
+    props: {
+      address: address,
+    },
+  };
+}
+
+const RequestIndex = ({ address }) => {
+  return (
+    <Layout>
+      <h3>Requests</h3>
+      <Link href={`/campaigns/${address}/requests/new`}>
+        <Button primary>Add Request</Button>
+      </Link>
+    </Layout>
+  );
 };
 
 export default RequestIndex;
